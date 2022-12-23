a3c0a73935feee73bfcddb82d29e3ea127966775
Yixiaoshan <119657085+Yixiaoshan@users.noreply.github.com>
2022-12-24 01:17:08 +0800

    Delete src directory

--- src/js/register.js
+++
@@ -1,7 +0,0 @@
-Vue.component('countdown', countdown)
-Vue.component('clock', clock)
-Vue.component('todo', todo)
-Vue.component('classlist', classlist)
-Vue.component('timer', timer)
-Vue.component('music', music)
-Vue.component('bilicard', bilicard)
